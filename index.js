12a5cecc633e9a6fbffcbec3d8613490637ffea8
Zander Martineau <zander@martineau.tv>
2015-09-13 09:42:24 +0100

    added more features:
    - `depdoc()` now accepts a 2nd argument which is used to signify that `json` has been used as this input rather than a file or from a url. Pass `'json'` as the 2nd param, like so: `depdoc({"name":"depdoc"},'json')`
    - we now check if the `package.json` even has any dependencies. If it doesn't a message is returned.
    - Mustache templating is used to generate the string

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  *
  * USAGE:
  * depdoc package.json ✔
- * depdoc https://raw.githubusercontent.com/TryKickoff/kickoff/master/package.json ✔
+ * depdoc https://raw.githubusercontent.com/mrmartineau/depdoc/master/package.json ✔
  *
  * TODO:
  * - Create an online version that takes json pasted in a textarea
@@ -12,24 +12,31 @@
  * - Error handling:
  *   - if the registry is down
  *   - if the path/filename is incorrect/doesn't exist
+ *   - if the package has no dependencies ✔
  */
 
 var fs = require('fs');
 var _ = require('lodash');
 var registryUrl = require('registry-url')();
 var request = require('sync-request');
+var Mustache = require('mustache');
 
-function depdoc(packageURL) {
+
+function depdoc(input, type) {
 	var result;
 
-	if (packageURL.indexOf('github') > 0) {
-		var newPackageUrl = packageURL.replace('raw.githubusercontent.com', 'rawgit.com');
+	if (type === 'json') {
+		result = getPackageInformation(input);
+
+	} else if (input.indexOf('github') > 0) {
+		var newPackageUrl = input.replace('raw.githubusercontent.com', 'rawgit.com');
 		var res = request('GET', newPackageUrl);
-		var data = res.getBody('utf-8').toString();
-		result = getPackageInformation(data);
+		var data = res.getBody('utf-8').toString2();
+		result = getPackageInformation(JSON.parse(data));
+
 	} else {
-		var readFile = fs.readFileSync(packageURL);
-		result = getPackageInformation(readFile);
+		var readFile = fs.readFileSync(input);
+		result = getPackageInformation(JSON.parse(readFile));
 	}
 
 	return result;
@@ -40,29 +47,33 @@ function depdoc(packageURL) {
  * Get package.json contents
  */
 function getPackageInformation(data) {
-	var deps = JSON.parse(data).dependencies;
+	var deps = data.dependencies;
 	var result = '';
 
-	_.forEach(deps,
-		function(value, key){
+	if (_.isEmpty(deps)) {
+		return 'This package has no dependencies :(';
+	}
 
-			var res = request('GET', registryUrl + key);
-			var data = res.getBody('utf-8').toString();
+	_.forEach(deps, function(value, key){
 
-			var packageInfo = JSON.parse(data);
-			var name        = packageInfo.name;
-			var description = packageInfo.description;
-			var homepage    = packageInfo.homepage;
-			var issues      = packageInfo.bugs.url;
-			var repo        = packageInfo.repository;
-			var license     = packageInfo.license;
+		var res = request('GET', registryUrl + key);
+		var data = res.getBody('utf-8').toString();
 
-			result += '## ['+ name +']('+ homepage +')' +
-			'\n'+ description +
-			'\n\n[npm](http://npmjs.org/'+ key +') - [Homepage]('+ homepage +') - [Repository]('+ repo.url +') ('+ repo.type +') - [Issues]('+ issues +') - Licence: '+ license +
-			'\n\nInstall with `npm install '+ name +'`' +
-			'\n---\n';
-		});
+		var packageInfo = JSON.parse(data);
+		var packageInfoData = {
+			name: packageInfo.name,
+			description: typeof packageInfo.description !== 'undefined' ? packageInfo.description : '',
+			homepage: typeof packageInfo.homepage !== 'undefined' ? packageInfo.homepage : '',
+			issues: typeof packageInfo.bugs !== 'undefined' ? packageInfo.bugs.url : '',
+			repo: typeof packageInfo.repository !== 'undefined' ? packageInfo.repository : '',
+			license: typeof packageInfo.license !== 'undefined' ? packageInfo.license : ''
+		}
+
+		// TODO: Improve this:
+		result += Mustache.render("## [{{name}}]({{{homepage}}})\n{{description}}\n\n[npm](http://npmjs.org/{{name}}){{#homepage}} - [Homepage]({{{homepage}}}){{/homepage}}{{#repo}} - [Repository]({{{repo.url}}}) ({{repo.type}}){{/repo}}{{#issues}} - [Issues]({{{issues}}}){{/issues}} - Licence: {{license}}\n\nInstall with `npm install {{name}}`\n---\n", packageInfoData);
+		// console.log(result);
+	});
+
 	return result;
 }
 
